eb6725395309bb970b9b3157642876c61c028624
Dev-Nkundineza <niyonzimadeus2002@gmail.com>
2022-06-21 10:05:15 +0200

    deplyoying the application

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import express from "express";
-import {ApolloServer} from "apollo-server-express";
-import { schema } from "./schema";
-import { context } from "./context";
+import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
-const startApolloServer = async () =>{
-    const server = new ApolloServer({
-        schema, 
-        context,
-        introspection: true,                                      // 1
-        plugins: [ApolloServerPluginLandingPageLocalDefault()],});
-    const app = express();
-    await server.start();
-    server.applyMiddleware({app});
-    const port = process.env.PORT || '4000'
-    app.listen({port: port},()=> console.log(`Server is running at http://localhost:4000${server.graphqlPath} `))
-}
-startApolloServer();
+import { schema } from "./schema";
+import { context } from "./context";
+
+export const server = new ApolloServer({
+    schema,
+    context,
+    introspection: true,                                      // 1
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],   // 2
+});
+
+const port = process.env.PORT || 3000;                        // 3
+
+server.listen({ port }).then(({ url }) => {
+    console.log(`🚀  Server  ready at ${url}`);
+});
